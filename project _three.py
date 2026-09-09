ffa9f032571bf89651584e12082d48a43c88ceab
mohamad1 <malhariri833@gmail.com>
2026-09-09 19:53:52 +0200

    done

---
+++ project _three.py
@@ -0,0 +1,19 @@
+team1=15
+team2=16
+team3=18
+team4=19
+team5=20
+total=team1+team2+team3+team4+team5
+print("the teams had scoord ",total," points")
+avrege=total/5
+print("averge scoor of the teams is ",avrege)
+rewards=5
+print("total rewerds of the teams is ",total*rewards)
+boxes=total*rewards//25
+leftover=total*rewards%25
+print("full boxes are",boxes)
+print("leftover stars are",leftover)
+last_week=80
+print(total > last_week)
+print(total == last_week)
+print(total >= last_week)
